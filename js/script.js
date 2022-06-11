cac00b1a55d755344aa4bdbc79922f024b54e378
Bruno Messias Aguiar <85034205+brunoaguiar1986@users.noreply.github.com>
2022-06-11 11:56:39 -0300

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,55 @@
+//.sobre
+
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.sobre', { duration: 1000 });
+
+window.sr = ScrollReveal();
+sr.reveal('.sobre', {
+    origin: 'left',
+    distance: '70rem',
+    duration: 1000,
+    delay: 100,
+    easing: 'ease-in-out'
+});
+
+//.projetos
+
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.projects-cards', { duration: 1000 });
+
+window.sr = ScrollReveal();
+sr.reveal('.projects-cards', {
+    origin: 'left',
+    distance: '70rem',
+    duration: 1000,
+    delay: 100,
+    easing: 'ease-in-out'
+});
+
+//.skills
+
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.skills-card', { duration: 1000 });
+
+window.sr = ScrollReveal();
+sr.reveal('.skills-card', {
+    origin: 'left',
+    distance: '70rem',
+    duration: 1000,
+    delay: 100,
+    easing: 'ease-in-out'
+});
+
+//.contato
+
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.contato', { duration: 1000 });
+
+window.sr = ScrollReveal();
+sr.reveal('.contato', {
+    origin: 'left',
+    distance: '70rem',
+    duration: 1000,
+    delay: 100,
+    easing: 'ease-in-out'
+});
