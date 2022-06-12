60b49664e2e52b1eb1a87edb782a141e5d1231a6
Bruno Messias Aguiar <85034205+brunoaguiar1986@users.noreply.github.com>
2022-06-12 15:19:52 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+//.s-home
+
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.s-home', { duration: 500 });
+
+window.sr = ScrollReveal();
+sr.reveal('.s-home', {
+    origin: 'left',
+    distance: '70rem',
+    duration: 500,
+    delay: 100,
+    easing: 'ease-in-out'
+});
+
+//.minha-foto
+
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.minha-foto', { duration: 1000 });
+
+window.sr = ScrollReveal();
+sr.reveal('.minha-foto', {
+    origin: 'left',
+    distance: '70rem',
+    duration: 1000,
+    delay: 100,
+    easing: 'ease-in-out'
+});
+
 //.sobre
 
 window.sr = ScrollReveal({ reset: true });
